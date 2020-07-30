387e2b5606b8ecd0d3c71c260f74c60f41b09f0a
sady1618 <sandeepnayak1.618@gmail.com>
2020-07-30 17:22:23 +0900

    untracked and retraced with .gitignore

--- rosws/simulation_ws/build/turtlebot/kobuki_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sandy/MicronUVGIRobot/rosws/simulation_ws/src/turtlebot/kobuki_description
-# Build directory: /home/sandy/MicronUVGIRobot/rosws/simulation_ws/build/turtlebot/kobuki_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- rosws/simulation_ws/build/turtlebot/multiple_kobukis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sandy/MicronUVGIRobot/rosws/simulation_ws/src/turtlebot/multiple_kobukis
-# Build directory: /home/sandy/MicronUVGIRobot/rosws/simulation_ws/build/turtlebot/multiple_kobukis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
