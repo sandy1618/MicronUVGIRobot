801db12eaa126ad0c9811798759b32c68ebaa90a
SHREYA SANTRA <Shreya.santra@skolkovotech.ru>
2020-08-29 03:20:17 +0900

    Updated
    Code to run on ATtiny85 for PIR and relay circuit for motion detection

--- PIR_Relay_code/PIR_Relay_code.ino
+++ PIR_Relay_code/PIR_Relay_code.ino
@@ -1,11 +1,20 @@
+/*Arduino IDE Sketch
+Key Hardware: Attiny85 Board, 12V Relay Switch & Generic PIR Sensor Module*/
+
 int sensorPin = 2;          // Set up a PIR sensor pin
+int stPin =1;               // the number of status indicator pin
 int pirState = LOW;
 int val = 0;
 int relayPin = 3;           //Set up a Relay pin
+int state = LOW;
+int ledState = LOW;         // For the LED indicators
 
 void setup() {
 pinMode(sensorPin, INPUT);
 pinMode(relayPin, OUTPUT);
+pinMode(stPin,OUTPUT);
+digitalWrite(sensorPin,HIGH); // internal pull-up option
+digitalWrite(stPin,HIGH);
 Serial.begin(9600);
 }
 
@@ -13,18 +22,24 @@ void loop() {
 val = digitalRead(sensorPin);  // read input value
 if (val == HIGH) {            // check if the input is HIGH
 digitalWrite(relayPin, HIGH);  // turn Relay ON
+ledState = HIGH;
 delay(150);
 
 if (pirState == LOW) {
 Serial.println("Motion detected!");
-pirState = HIGH;
+pirState = HIGH;  
+ledState = LOW;  // turn Relay OFF - UV tubes and LED indicators turn off
 }
 } else {
-digitalWrite(relayPin, LOW); // turn Relay OFF - UV tubes and LED indicators turn off
+digitalWrite(relayPin, LOW); // turn Relay OFF - UV tubes and LED indicators are off
 delay(150); //stay off for atleast 15 seconds
+state = HIGH;
+ledState = LOW; // status indicator mode - optional
 if (pirState == HIGH) {
 Serial.println("Motion ended!");
 pirState = LOW;
+ledState = HIGH;
 }
 }
 }
+// End Of Code
