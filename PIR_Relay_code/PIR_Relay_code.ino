c5daf200d31ccfc6fcea61a6264ebe5876a86b9a
SRL <shreya.santra@skolkovotech.ru>
2020-08-29 02:43:11 +0900

    Code to run on ATtiny85 for PIR and relay circuit for motion detection

---
+++ PIR_Relay_code/PIR_Relay_code.ino
@@ -0,0 +1,30 @@
+int sensorPin = 2;          // Set up a PIR sensor pin
+int pirState = LOW;
+int val = 0;
+int relayPin = 3;           //Set up a Relay pin
+
+void setup() {
+pinMode(sensorPin, INPUT);
+pinMode(relayPin, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+val = digitalRead(sensorPin);  // read input value
+if (val == HIGH) {            // check if the input is HIGH
+digitalWrite(relayPin, HIGH);  // turn Relay ON
+delay(150);
+
+if (pirState == LOW) {
+Serial.println("Motion detected!");
+pirState = HIGH;
+}
+} else {
+digitalWrite(relayPin, LOW); // turn Relay OFF
+delay(150);
+if (pirState == HIGH) {
+Serial.println("Motion ended!");
+pirState = LOW;
+}
+}
+}
